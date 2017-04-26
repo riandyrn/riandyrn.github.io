a133cd7946138d0cb93ae961b10bb6928cdc6aed
Riandy Rahman Nugraha <riandyrn@gmail.com>
2017-04-26 17:19:13 +0700

    push latest template + google vision api result

---
+++ RekognitionResultTemplate/yom.py
@@ -0,0 +1,1 @@
+result.append({'path': image_path, 'rekognition_result': rekognition_result, 'is_violating_image': violation_check['is_violating'], 'recognized_label': violation_check['recognized_label']})
